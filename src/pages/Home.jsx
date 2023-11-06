882761372a3fa0f51e595dc89a8b5a9e54c28e8c
Reus <victor.villalva2003@hotmail.com>
2023-11-05 23:19:25 -0600

    New TextArea

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import '../assets/Styles/home.css'
 import G from '../assets/Images/Icono-Language.svg'
 import Git from '../assets/Images/GitHub.svg'
-import { Input, Button } from '@mui/material'
+import { Input, Button, TextField } from '@mui/material'
 
 
 
@@ -23,77 +23,93 @@ const Home = () => {
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                             <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                             <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                             <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                             <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                         </div>
                         <div className="col-2">
-                        <form action="">
+                            <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                             <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                             <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                             <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                             <form action="">
                                 <Input fullWidth placeholder="Placeholder"
                                     style={{ color: 'white', fontFamily: 'Raleway', borderBottom: '#FFFFFF 1px solid', marginTop: '2rem', }}
                                 />
-                                <Button variant="contained" style={{backgroundColor:'white', color:'#131313', marginTop:'1rem'}}>Revisar</Button>
+                                <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
                             </form>
                         </div>
                     </div>
                 </article>
+                <div className="textValidationCode">
+                    <h2 className="codeValidation">Validacion de codigo</h2>
+                    <div className="textArea">
+                        <TextField
+                            fullWidth
+                            id="outlined-multiline-static"
+                            label=""
+                            multiline
+                            rows={4}
+                            defaultValue="Ingresa"
+                            style={{backgroundColor: 'white', border:'#FFFFFF 1px solid', fontFamily: 'Raleway',}}
+                            color='secondary'
+                        />
+                    </div>
+                    <Button variant="contained" style={{ backgroundColor: 'white', color: '#131313', marginTop: '1rem' }}>Revisar</Button>
+                </div>
                 <footer>
                     <div className="logo-garrick">
                         <img src={G} alt="" />
                     </div>
                     <div className="github">
                         <div className="victor">
-                            <img src={Git} alt="" className='gtb'/><label htmlFor="github" className='name'><a href="https://github.com/VictorVillalva">Victor Adrian Villalva Rodriguez</a> 213359</label>
+                            <img src={Git} alt="" className='gtb' /><label htmlFor="github" className='name'><a href="https://github.com/VictorVillalva">Victor Adrian Villalva Rodriguez</a> 213380</label>
                         </div>
                         <div className="jesus">
-                            <img src={Git} alt="" className='gtb'/><label htmlFor="github" className='name'><a href="https://github.com/Gsuscrist">Jesus Antonio Gordillo Orantes</a> 213359</label>
+                            <img src={Git} alt="" className='gtb' /><label htmlFor="github" className='name'><a href="https://github.com/Gsuscrist">Jesus Antonio Gordillo Orantes</a> 213359</label>
                         </div>
                     </div>
                 </footer>
