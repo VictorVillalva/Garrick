10e9dfcbac47bebc66b26ef527f7fc3a27c70744
Reus <victor.villalva2003@hotmail.com>
2023-11-22 19:37:10 -0600

    Inputs and Home new

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,10 @@
-import '../assets/Styles/form.css';
-import { TextField } from '@mui/material';
 
-const Form = () => {
+function Form() {
     return (
-        <div className='container-form'>
-            <section className="information">
-                <div className="data">
-                    {/* <h2>Ingresa {type} para comprobar</h2>
-                    <div className="input-data">
-                        <input type="text" /> */}
-                        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                        <TextField id="filled-basic" label="Filled" variant="filled" />
-                        <TextField id="standard-basic" label="Standard" variant="standard" />
-                    {/* </div> */}
-                </div>
-            </section>
+        <div>
+            <h1>Hola mundo</h1>
         </div>
-    )
+    );
 }
 
-export default Form
+export default Form;
